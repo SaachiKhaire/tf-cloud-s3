e5b8c1f01019de5184095c1465a06347f470cd12
Sachi Khaire <sachikhaire909@gmail.com>
2026-01-14 15:38:34 +0530

    Adding tf config for S3

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+     random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+ 
+resource "random_id" "rand-id"{
+    byte_length = 8
+  
+}
+resource "aws_s3_bucket" "demo-bucket" {
+    bucket = "tf-cloud-${random_id.rand-id.hex}"
+
+}
